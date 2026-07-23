11c2dd59d9406d645138d1f11252c70ee4cfaa97
Usama Baig <usamabaig.skncoe.comp@gmail.com>
2026-07-23 17:12:32 +0530

    invoices page and authentication done

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,32 @@
+import Form from "@/app/ui/invoices/edit-form";
+import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
+import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { notFound } from "next/navigation";
+
+export default async function Page(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+  if(!invoice){
+    notFound();
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: "Invoices", href: "/dashboard/invoices" },
+          {
+            label: "Edit Invoice",
+            href: `/dashboard/invoices/${id}/edit`,
+            // href: `/dashboard/invoices/2e94d1ed-d220-449f-9f11-f0bbceed9645/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
